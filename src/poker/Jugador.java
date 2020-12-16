55ad9c63ddbf1766064f0f8e40f64a3a40d546fe
CRISTOBAL VELASCO VILCHEZ <71633086+cristobalvelasco8@users.noreply.github.com>
2020-12-16 20:44:48 +0100

    add

--- src/poker/Jugador.java
+++ src/poker/Jugador.java
@@ -32,6 +32,43 @@ public class Jugador {
 	public String toString() {
 		return "Jugador [nombreJugador=" + nombreJugador + ", mano=" + Arrays.toString(mano) + "]";
 	}
+	
+	public int verificarPuntosMano () {
+		int maximoPuntos = 1;
+		for (int i = 0; i < mano.length-1; i++) {
+			int contador = 1;
+			for (int j = i+1; j < mano.length; j++) {
+				if(mano[i].getValor() == mano[j].getValor())
+					contador++;
+			}
+			if(contador > maximoPuntos)
+				maximoPuntos = contador;
+		}
+		return maximoPuntos;
+	}
+	
+	public void jugada() {
+		switch (verificarPuntosMano()) {
+		case 1:
+			System.out.println("Soy el " + this.nombreJugador + " y tengo solo carta alta");
+			break;
+		case 2:
+			System.out.println("Soy el " + this.nombreJugador + " y tengo"
+					+ " una pareja");
+			break;
+		case 3:
+			System.out.println("Soy el " + this.nombreJugador + " y tengo "
+					+ "un trio");
+			break;
+		case 4:
+			System.out.println("Soy el " + this.nombreJugador + " y tengo"
+					+ " un poker");
+			break;
+		default:
+			System.out.println("no tengo nada");
+			break;
+		}
+	}
 }
 
 	
