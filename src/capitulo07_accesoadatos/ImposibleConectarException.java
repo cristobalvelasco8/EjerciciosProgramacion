29904b9dcaca5feb122e31138a9b5b6e69259787
CRISTOBAL VELASCO VILCHEZ <71633086+cristobalvelasco8@users.noreply.github.com>
2021-03-17 17:17:38 +0100

    add

---
+++ src/capitulo07_accesoadatos/ImposibleConectarException.java
@@ -0,0 +1,10 @@
+package capitulo07_accesoadatos;
+
+public class ImposibleConectarException extends Exception {
+
+	public ImposibleConectarException(String message) {
+		super(message);
+	}
+
+	
+}
